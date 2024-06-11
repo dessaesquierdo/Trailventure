fa067ac4e438be633abf84fb8ba4f01257c823b7
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2024-06-11 08:52:19 +0800

    fix: rename attributes to jsx attributes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -53,17 +53,17 @@ function Signup() {
               Password
             </label>
 
-            <p class="flex items-center gap-1 mt-2 font-sans text-sm antialiased font-normal leading-normal text-red-200">
+            <p className="flex items-center gap-1 mt-2 font-sans text-sm antialiased font-normal leading-normal text-red-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-4 h-4 -mt-px"
+                className="w-4 h-4 -mt-px"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               Use at least 8 characters, one uppercase, one lowercase and one
