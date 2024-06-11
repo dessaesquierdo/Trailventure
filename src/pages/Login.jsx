1a855ac571073f26cffab9d4c345567b593706ff
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2024-06-11 13:38:11 +0800

    fix: disable input fields when form is submitting

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -111,6 +111,7 @@ function Login() {
         <div className="relative mt-4">
           <input
             placeholder="Email"
+            disabled={loading}
             className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
             {...register("email", {
               required: "Please enter your email",
@@ -138,6 +139,7 @@ function Login() {
           <input
             type="password"
             placeholder="Password"
+            disabled={loading}
             className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
             {...register("password", {
               required: "Please enter your password",
